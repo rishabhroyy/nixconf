9168128457adedd3d99f0110b20816e9387bfc29
Rishabh <61441648+rishabhroyy@users.noreply.github.com>
2026-05-05 23:50:47 -0700

    fix qemu mismatch

--- modules/kvm-hypercall-patch/kvm-hypercall-patch.c
+++ modules/kvm-hypercall-patch/kvm-hypercall-patch.c
@@ -7,14 +7,21 @@
 #include <linux/kallsyms.h>
 #include <linux/kprobes.h>
 
+typedef void (*kvm_queue_exception_fn_t)(struct kvm_vcpu *vcpu, unsigned nr);
+static kvm_queue_exception_fn_t kvm_queue_exception_resolved;
+
 static struct kprobe kp_hypercall = {
     .symbol_name = "kvm_emulate_hypercall",
+};
+
+static struct kprobe kp_queue_exception = {
+    .symbol_name = "kvm_queue_exception",
 };
 
 static int ud_hypercall(struct kvm_vcpu *vcpu, int cpl,
                        int (*complete_hypercall)(struct kvm_vcpu *))
 {
-    kvm_queue_exception(vcpu, UD_VECTOR);
+    kvm_queue_exception_resolved(vcpu, UD_VECTOR);
     return 1;
 }
 
@@ -35,30 +42,53 @@ static struct ftrace_ops hypercall_ops = {
 };
 
 
-static int __init init(void) {
+static int __init kvm_hypercall_patch_init(void) {
+    int ret;
+
     printk(KERN_INFO "Registering hypercall ud hook\n");
 
-    register_kprobe(&kp_hypercall);
+    ret = register_kprobe(&kp_queue_exception);
+    if (ret < 0) {
+        printk(KERN_ERR "Failed to resolve kvm_queue_exception: %d\n", ret);
+        return ret;
+    }
+    kvm_queue_exception_resolved = (kvm_queue_exception_fn_t)kp_queue_exception.addr;
+    unregister_kprobe(&kp_queue_exception);
+
+    ret = register_kprobe(&kp_hypercall);
+    if (ret < 0) {
+        printk(KERN_ERR "Failed to resolve kvm_emulate_hypercall: %d\n", ret);
+        return ret;
+    }
     hypercall_addr = (unsigned long)kp_hypercall.addr;
     unregister_kprobe(&kp_hypercall);
 
-    ftrace_set_filter_ip(&hypercall_ops, hypercall_addr, 0, 0);
-    register_ftrace_function(&hypercall_ops);
+    ret = ftrace_set_filter_ip(&hypercall_ops, hypercall_addr, 0, 0);
+    if (ret) {
+        printk(KERN_ERR "Failed to set hypercall ftrace filter: %d\n", ret);
+        return ret;
+    }
+
+    ret = register_ftrace_function(&hypercall_ops);
+    if (ret) {
+        printk(KERN_ERR "Failed to register hypercall ftrace hook: %d\n", ret);
+        ftrace_set_filter_ip(&hypercall_ops, hypercall_addr, 1, 0);
+        return ret;
+    }
 
     return 0;
 }
 
-static void __exit exit(void) {
+static void __exit kvm_hypercall_patch_exit(void) {
     printk(KERN_INFO "Unregistering hypercall ud hook\n");
     unregister_ftrace_function(&hypercall_ops);
     ftrace_set_filter_ip(&hypercall_ops, hypercall_addr, 1, 0);
 }
 
-module_init(init);
-module_exit(exit);
+module_init(kvm_hypercall_patch_init);
+module_exit(kvm_hypercall_patch_exit);
 
 MODULE_LICENSE("GPL");
-MODULE_IMPORT_NS("KVM_AMD");
-MODULE_IMPORT_NS("KVM_INTEL");
 MODULE_AUTHOR("Pantae");
 MODULE_DESCRIPTION("Patch hypercall (vmmcall or vmcall) to raise UD");
+MODULE_SOFTDEP("pre: kvm-amd");
