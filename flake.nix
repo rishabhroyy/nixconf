5d1bafd2d28891482192cec5b32165512a939468
Rishabh <61441648+rishabhroyy@users.noreply.github.com>
2026-05-06 23:25:34 -0700

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -39,8 +39,7 @@ static void set_v8086_seg'
 }
 
 static bool tsc_is_stable_and_known' '    if (env->tsc_khz) {
-        info_report_once("qemu-acpi-oem-patch=ALASKA,A M I");
-        info_report("tsc-scaling-patch: guest TSC frequency set to %" PRId64 " kHz",
+        info_report("tsc-scaling-patch: guest TSC frequency set to %" PRId64 " kHz; qemu-acpi-oem-patch=ALASKA,A M I",
                     env->tsc_khz);
     }
 
